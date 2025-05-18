b557af1d245aa44a56e2c3e3c543e959939d7cf3
enosaf7 <128220498+enosaf7@users.noreply.github.com>
2025-05-18 08:05:55 +0000

    Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,17 +1,31 @@
+import React from "react";
+import Navbar from "@/components/layout/Navbar";
 
-import Navbar from "./Navbar";
-import Footer from "./Footer";
-
-interface LayoutProps {
+type Props = {
   children: React.ReactNode;
-}
-
-export const Layout = ({ children }: LayoutProps) => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
-  );
 };
+
+/**
+ * Adjust NAVBAR_HEIGHT if your navbar is taller/shorter.
+ * If using Tailwind, pt-16 = 4rem = 64px.
+ */
+const NAVBAR_HEIGHT = 64; // in px
+
+const Layout: React.FC<Props> = ({ children }) => (
+  <>
+    <Navbar />
+    <main
+      // For Tailwind users, you can use: className="min-h-screen pt-16 px-4"
+      style={{
+        minHeight: "100vh",
+        paddingTop: NAVBAR_HEIGHT,
+        paddingLeft: "1rem",
+        paddingRight: "1rem"
+      }}
+    >
+      {children}
+    </main>
+  </>
+);
+
+export { Layout };
